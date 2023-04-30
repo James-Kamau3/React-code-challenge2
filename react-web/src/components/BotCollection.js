8e1a012fb565d2251fe66b54014417b7760b069d
James <kamaujames3705@gmail.com>
2023-05-01 00:21:52 +0300

    Displaying bots data in DOM

--- react-web/src/components/BotCollection.js
+++ react-web/src/components/BotCollection.js
@@ -1,33 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
+//import BotCard from './BotCard';
 
-function BotCollection() {
+function BotsCollection() {
+  const [botsData, setBotsData] = useState([]);
 
-    const [bots, setBots] = useState([]);
-
-    useEffect = (() => {
+  useEffect(() => {
     fetch('http://localhost:8001/bots')
-    .then(response => response.json())
-    .then(data => setBots(data))
-    .catch(error => console.log(error));
-    }, []);
-    
+      .then(response => response.json())
+      .then(data => setBotsData(data))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div className='bots-collection' >
-        {bots.map(bot => <div key = {bot.id}>
-            <ul>
-                <li>
-                    {bot.name}
-                </li>
-                <li>
-                    {bot.health}
-                </li>
-
-            </ul>
-        </div>)}
+    <div className="bots-collection">
+      {botsData.map(bot => <div key={bot.id} bot={bot} >
+        <ul>
+            <li>
+                {bot.name}
+            </li>
+            <li>
+                {bot.health}
+            </li>
+            <li>
+                {bot.damage}
+            </li>
+            <li>
+                {bot.armor}
+            </li>
+            <li>
+                {bot.bot_class}
+            </li>
+            <li>
+                {bot.catchphrase}
+            </li>
+            <li>
+                <img src={bot.avatar_url} alt='avatar'/>
+            </li>
+            <li>
+                {bot.created_at}
+            </li>
+            <li>
+                {bot.updated_at}
+            </li>
+        </ul>
+      </div>)}
     </div>
-    
-
-  )
+  );
 }
 
-export default BotCollection;
+export default BotsCollection;
+
