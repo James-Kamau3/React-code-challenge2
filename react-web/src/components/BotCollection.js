c0b743a832f6864564047e09d5045b7f9a1603ec
James <kamaujames3705@gmail.com>
2023-05-01 01:07:43 +0300

    Displaying bots data in DOM

--- react-web/src/components/BotCollection.js
+++ react-web/src/components/BotCollection.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-//import BotCard from './BotCard';
+import '../App.css'
 
 function BotsCollection() {
   const [bots, setData] = useState([]);
@@ -13,32 +13,41 @@ function BotsCollection() {
 
   return (
     <div className="bots-collection">
-      {bots.map(bot => <div key={bot.id} bot={bot} >
+      {bots.map(bot => <div className = 'each' key={bot.id} bot={bot} >
         <ul>
+            <h5 className='label'>Name: </h5>
             <li>
                 {bot.name}
             </li>
+            <h5 className='label'>Health: </h5>
             <li>
                 {bot.health}
             </li>
+            <h5 className='label'>Damage: </h5>
             <li>
                 {bot.damage}
             </li>
+            <h5 className='label'>Armor: </h5>
             <li>
                 {bot.armor}
             </li>
+            <h5 className='label'>Bot_Class: </h5>
             <li>
                 {bot.bot_class}
             </li>
+            <h5 className='label'>Catchphtase: </h5>
             <li>
                 {bot.catchphrase}
             </li>
+            <h5 className='label'>Avatar_Image: </h5>
             <li>
                 <img src={bot.avatar_url} alt='avatar'/>
             </li>
+            <h5 className='label'>Created_At: </h5>
             <li>
                 {bot.created_at}
             </li>
+            <h5 className='label'>Updated_At: </h5>
             <li>
                 {bot.updated_at}
             </li>
