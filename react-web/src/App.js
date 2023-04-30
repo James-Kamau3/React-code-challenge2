6215cd6f1819faef74630b4eab7d965e022275a4
James <kamaujames3705@gmail.com>
2023-05-01 01:12:13 +0300

    CSS property to set h1 centre

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,10 +1,10 @@
-//import './App.css'
+import './App.css'
 import BotCollection from "./components/BotCollection";
 
 function App() {
   return (
     <div>
-      <h1>Bots</h1>
+      <h1 id='bots'>Bots</h1>
       <BotCollection />
     </div>
   );
