8651a8468503c2030c7c2bf78ed1772f5bfc6940
James <kamaujames3705@gmail.com>
2023-05-01 00:06:38 +0300

    Mapping bots data

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,9 +1,11 @@
-//import './App.css';
+//import './App.css'
+import BotCollection from "./components/BotCollection";
 
 function App() {
   return (
     <div>
-      
+      <h1>Bots</h1>
+      <BotCollection />
     </div>
   );
 }
--- react-web/src/components/BotCollection.js
+++ react-web/src/components/BotCollection.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function BotCollection() {
+
+    const [bots, setBots] = useState([]);
+
+    useEffect = (() => {
+    fetch('http://localhost:8001/bots')
+    .then(response => response.json())
+    .then(data => setBots(data))
+    .catch(error => console.log(error));
+    }, []);
+    
   return (
-    <div>
-      
+    <div className='bots-collection' >
+        {bots.map(bot => <div key = {bot.id}>
+            <ul>
+                <li>
+                    {bot.name}
+                </li>
+                <li>
+                    {bot.health}
+                </li>
+
+            </ul>
+        </div>)}
     </div>
+    
+
   )
 }
 
